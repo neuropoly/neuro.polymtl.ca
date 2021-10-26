fb4a81aaae8c32ea142edd27264bcacd1e5837d9
Nick <nick@kousu.ca>
2021-10-26 12:41:50 -0400

    Replace fullscreen/download with Edit On Github

--- conf.py
+++ conf.py
@@ -51,3 +51,10 @@ html_theme = 'sphinx_book_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    "repository_url": "https://github.com/neuropoly/neuropoly-docs",
+    "use_fullscreen_button": False,
+    "use_edit_page_button": True,
+    "use_download_button": False,
+}
