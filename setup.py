53e52dbadb47d9dc3ac4128ebdd865c7a0ecbca5
RignonNoel <noel.rignon@fjnr.ca>
2022-08-23 13:03:24 -0400

    add fr locale for i18n

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
             "pandas",
             "sphinx-book-theme",
             "sphinx-panels",
+            "sphinx-intl",
             "sphinx-design~=0.0.11", # pinned because of this bug https://github.com/pydata/pydata-sphinx-theme/pull/50# and that the patched sphinx-book-theme isn't out yet: https://github.com/executablebooks/sphinx-book-theme/issues/428#issuecomment-966021270
             "sphinx~=4.2.0", # TODO: unpin when the next sphinx-book-theme is released
         ],
