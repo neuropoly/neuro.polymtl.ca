073adbc9b9960082fa01c039cc7d918581f65309
Nathan <nathan@nzm.ca>
2023-06-05 11:50:46 -0400

    conf will now put js+css for sorting into the generated file

--- conf.py
+++ conf.py
@@ -213,7 +213,7 @@ def build_gallery(app: Sphinx):
     (Path(app.srcdir) / "gallery_software.txt").write_text(panels)
 
 def build_publications(app: Sphinx, pagename, templatename, context, doctree):
-    if pagename == "publications/journal_articles":
+    if pagename == "publications/articles":
         app.add_js_file("js/custom.js")
         app.add_css_file("css/custom.css")
 
